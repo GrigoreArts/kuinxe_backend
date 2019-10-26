93ccb68d6d3f6f10addc16942e00f1daf8ba6b9a
Me <me@pop-os.localdomain>
2019-10-26 15:21:29 +0200

    Implemented now in actix

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
-use std::fs;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello World Again!")
+}
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
-    };
-
-        let contents = fs::read_to_string(filename).unwrap();
-
-        let response = format!("{}{}", status_line, contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    
-
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind("127.0.0.1:7878")
+    .unwrap()
+    .run()
+    .unwrap();
 }
